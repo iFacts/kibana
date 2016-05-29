f8730bb56d490167988c8d194da41680077a5f81
Sebastian Nordin <sebastian.nordin@ifacts.se>
2016-05-29 22:38:43 +0200

    Add culture support for numbers and dates - Uses window.navigator.userLanguage, which later should be changed to use user provided localization and language settings

--- webpackShims/numeral.js
+++ webpackShims/numeral.js
@@ -1,1 +1,27 @@
-module.exports = require('@spalger/numeral');
+var numeral = module.exports = require('@spalger/numeral');
+var locale = window.navigator.userLanguage || window.navigator.language || 'en';
+
+if (locale === 'sv') {
+  var languageData = {
+    delimiters: {
+      thousands: ' ',
+      decimal: ','
+    },
+    abbreviations: {
+      thousand: 'k',
+      million: 'M',
+      billion: 'G',
+      trillion: 'T'
+    },
+    ordinal: function (number) {
+      var str = number.toString();
+      var endsWith = str[str.length - 1];
+      return (endsWith === '1' || endsWith === '2') ? ':a' : ':e';
+    },
+    currency: {
+      symbol: 'SEK'
+    }
+  };
+  numeral.language(locale, languageData);
+  numeral.language(locale);
+}
